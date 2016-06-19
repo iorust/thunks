7ca066edc1109249c044f6966034b1eff8e0f38c
zensh <admin@zensh.com>
2016-06-19 17:58:28 +0800

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+thunks
+====
+Asynchronous composer for Rust.
+
+[![Crates version][version-image]][version-url]
+[![Build Status][travis-image]][travis-url]
+[![Coverage Status][coveralls-image]][coveralls-url]
+[![Crates downloads][downloads-image]][downloads-url]
+
+## API
+### Documentation https://iorust.github.io/thunks/thunks
+
+```Rust
+extern crate thunks;
+use thunks::Thunk;
+```
+
+
+[version-image]: https://img.shields.io/crates/v/thunks.svg
+[version-url]: https://crates.io/crates/thunks
+
+[travis-image]: http://img.shields.io/travis/iorust/thunks.svg
+[travis-url]: https://travis-ci.org/iorust/thunks
+
+[coveralls-image]: https://coveralls.io/repos/github/iorust/thunks/badge.svg?branch=master
+[coveralls-url]: https://coveralls.io/github/iorust/thunks?branch=master
+
+[downloads-image]: https://img.shields.io/crates/d/thunks.svg
+[downloads-url]: https://crates.io/crates/thunks
---
+++ src/lib.rs
@@ -0,0 +1,46 @@
+
+// use std::thread;
+use std::sync::{mpsc};
+use std::boxed::{Box};
+// use std::vec::Vec;
+
+pub struct Thunk<T, E>(Box<Fn(&(Fn(Result<T, E>) + Send)) + 'static>);
+
+impl<T, E> Thunk<T, E> where T: 'static + Sync + Send, E: 'static + Sync + Send {
+    pub fn new<F>(task: F) -> Thunk<T, E>
+        where F: Fn(&(Fn(Result<T, E>) + Send)) + 'static
+    {
+        Thunk(Box::new(task))
+    }
+
+    pub fn await(&self) -> Result<T, E> {
+        self.call_thunk()
+    }
+
+    fn call_thunk(&self) -> Result<T, E> {
+        let (tx, rx) = mpsc::channel::<Result<T, E>>();
+        (self.0)(&move |res| {
+            tx.send(res).unwrap();
+        });
+        rx.recv().unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // use std::thread;
+    use super::*;
+
+    #[test]
+    fn thunk() {
+        let t: Thunk<i32, i32> = Thunk::new(|cb| {
+            // thread::spawn(move|| {
+            //     cb(Ok(1 as i32));
+            // });
+            cb(Ok(1));
+        });
+        let res = t.await().unwrap();
+        // println!("{:?}", res);
+        assert_eq!(res, 1);
+    }
+}
