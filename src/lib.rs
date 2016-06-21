6a610b545c06734a6d44ad666d692b89ccf97231
zensh <admin@zensh.com>
2016-06-21 23:18:03 +0800

    use FnOnce

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,16 @@
        html_playground_url = "https://play.rust-lang.org",
        issue_tracker_base_url = "https://github.com/iorust/thunks/issues")]
 
+#![feature(unboxed_closures)]
+#![feature(fn_traits)]
+#![feature(fnbox)]
+
 //! Asynchronous composer for Rust.
 
 // use std::thread;
 use std::sync::mpsc::{Receiver, sync_channel};
-use std::boxed::{Box};
+use std::boxed::{Box, FnBox};
+use std::ops::FnOnce;
 
 pub struct Thunk<T, E>(Box<Fn(Box<Fn(Result<T, E>) + Send + 'static>) + Send + 'static>);
 
@@ -18,53 +23,73 @@ impl<T, E> Thunk<T, E> where T: Send + 'static, E: Send + 'static {
         Thunk(Box::new(task))
     }
 
-    pub fn seq(thunk_vec: Vec<Thunk<T, E>>) -> Thunk<Vec<T>, E> {
-        let thunk_vec = Box::new(thunk_vec);
-        Thunk::new(move |cb| {
-            let mut res: Vec<T> = Vec::new();
-            for thunk in thunk_vec.iter() {
-                match thunk.await() {
-                    Ok(val) => res.push(val),
-                    Err(err) => {
-                        cb(Err(err));
-                        return;
-                    }
-                }
-            }
-            cb(Ok(res));
-        })
-    }
+    // pub fn seq(thunk_vec: Vec<Thunk<T, E>>) -> Thunk<Vec<T>, E> {
+    //     let thunk_vec = Box::new(thunk_vec);
+    //     Thunk::new(move |cb| {
+    //         let mut res: Vec<T> = Vec::new();
+    //         for thunk in thunk_vec.into_iter() {
+    //             match thunk() {
+    //                 Ok(val) => res.push(val),
+    //                 Err(err) => {
+    //                     cb(Err(err));
+    //                     return;
+    //                 }
+    //             }
+    //         }
+    //         cb(Ok(res));
+    //     })
+    // }
 
-    pub fn all(thunk_vec: Vec<Thunk<T, E>>) -> Thunk<Vec<T>, E> {
-        let thunk_vec = Box::new(thunk_vec);
-        Thunk::new(move |cb| {
-            let mut res: Vec<T> = Vec::new();
-            let rx_vec: Vec<Receiver<Result<T, E>>> = thunk_vec.iter()
-                .map(|t| t.call_thunk()).collect();
+    // pub fn all(thunk_vec: Vec<Thunk<T, E>>) -> Thunk<Vec<T>, E> {
+    //     let thunk_vec = Box::new(thunk_vec);
+    //     Thunk::new(move |cb| {
+    //         let mut res: Vec<T> = Vec::new();
+    //         let rx_vec: Vec<Receiver<Result<T, E>>> = thunk_vec.iter()
+    //             .map(|t| t.call_thunk()).collect();
+    //
+    //         for rx in rx_vec.iter() {
+    //             match rx.recv().unwrap() {
+    //                 Ok(val) => res.push(val),
+    //                 Err(err) => {
+    //                     cb(Err(err));
+    //                     return;
+    //                 }
+    //             }
+    //         }
+    //         cb(Ok(res));
+    //     })
+    // }
 
-            for rx in rx_vec.iter() {
-                match rx.recv().unwrap() {
-                    Ok(val) => res.push(val),
-                    Err(err) => {
-                        cb(Err(err));
-                        return;
-                    }
-                }
-            }
-            cb(Ok(res));
-        })
-    }
+    // pub fn await(&self) -> Result<T, E> {
+    //     self.call_thunk().recv().unwrap()
+    // }
+    //
+    // fn call_thunk(&self) -> Receiver<Result<T, E>> {
+    //     let (tx, rx) = sync_channel::<Result<T, E>>(1);
+    //     (self.0)(Box::new(move |res| {
+    //         tx.try_send(res).unwrap();
+    //     }));
+    //     rx
+    // }
+}
 
-    pub fn await(&self) -> Result<T, E> {
-        self.call_thunk().recv().unwrap()
-    }
+// impl<'a, A, R> FnOnce<A> for Box<FnBox<A, Output = R> + 'a> {
+//     type Output = R;
+//
+//     extern "rust-call" fn call_once(self, args: A) -> R {
+//         self.call_box(args)
+//     }
+// }
 
-    fn call_thunk(&self) -> Receiver<Result<T, E>> {
+impl<T, E> FnOnce<()> for Thunk<T, E> where T: Send + 'static, E: Send + 'static {
+    type Output = Result<T, E>;
+    extern "rust-call" fn call_once(self, args: ()) -> Self::Output {
         let (tx, rx) = sync_channel::<Result<T, E>>(1);
-        (self.0)(Box::new(move |res| {
+        let cb: Box<Fn(Result<T, E>) + Send + 'static> = Box::new(move |res| {
             tx.try_send(res).unwrap();
-        }));
-        rx
+        });
+        (self.0)(cb);
+        rx.recv().unwrap()
     }
 }
 
@@ -72,6 +97,7 @@ impl<T, E> Thunk<T, E> where T: Send + 'static, E: Send + 'static {
 mod tests {
     use std::thread;
     use std::time::Duration;
+    use std::boxed::{Box, FnBox};
     use super::*;
 
     #[test]
@@ -82,60 +108,60 @@ mod tests {
                 cb(Ok(1));
             });
         });
-        let res = thunk.await().unwrap();
-        // println!("{:?}", res);
+        let res = thunk().unwrap();
+        println!("{:?}", res);
         assert_eq!(res, 1);
     }
 
-    #[test]
-    fn thunk_seq() {
-        let thunk_vec: Vec<Thunk<i32, &str>> = vec![
-            Thunk::new(|cb| {
-                thread::spawn(move || {
-                    thread::sleep(Duration::new(1, 0));
-                    cb(Ok(1));
-                });
-            }),
-            Thunk::new(|cb| {
-                thread::spawn(move || {
-                    thread::sleep(Duration::new(1, 0));
-                    cb(Ok(2));
-                });
-            }),
-            Thunk::new(|cb| {
-                thread::spawn(move || {
-                    thread::sleep(Duration::new(1, 0));
-                    cb(Ok(3));
-                });
-            })
-        ];
-        let res = Thunk::seq(thunk_vec).await().unwrap();
-        assert_eq!(res, vec![1, 2, 3]);
-    }
-
-    #[test]
-    fn thunk_all() {
-        let thunk_vec: Vec<Thunk<i32, &str>> = vec![
-            Thunk::new(|cb| {
-                thread::spawn(move || {
-                    thread::sleep(Duration::new(1, 0));
-                    cb(Ok(1));
-                });
-            }),
-            Thunk::new(|cb| {
-                thread::spawn(move || {
-                    thread::sleep(Duration::new(1, 0));
-                    cb(Ok(2));
-                });
-            }),
-            Thunk::new(|cb| {
-                thread::spawn(move || {
-                    thread::sleep(Duration::new(1, 0));
-                    cb(Ok(3));
-                });
-            })
-        ];
-        let res = Thunk::all(thunk_vec).await().unwrap();
-        assert_eq!(res, vec![1, 2, 3]);
-    }
+    // #[test]
+    // fn thunk_seq() {
+    //     let thunk_vec: Vec<Thunk<i32, &str>> = vec![
+    //         Thunk::new(|cb| {
+    //             thread::spawn(move || {
+    //                 thread::sleep(Duration::new(1, 0));
+    //                 cb(Ok(1));
+    //             });
+    //         }),
+    //         Thunk::new(|cb| {
+    //             thread::spawn(move || {
+    //                 thread::sleep(Duration::new(1, 0));
+    //                 cb(Ok(2));
+    //             });
+    //         }),
+    //         Thunk::new(|cb| {
+    //             thread::spawn(move || {
+    //                 thread::sleep(Duration::new(1, 0));
+    //                 cb(Ok(3));
+    //             });
+    //         })
+    //     ];
+    //     let res = Thunk::seq(thunk_vec).await().unwrap();
+    //     assert_eq!(res, vec![1, 2, 3]);
+    // }
+    //
+    // #[test]
+    // fn thunk_all() {
+    //     let thunk_vec: Vec<Thunk<i32, &str>> = vec![
+    //         Thunk::new(|cb| {
+    //             thread::spawn(move || {
+    //                 thread::sleep(Duration::new(1, 0));
+    //                 cb(Ok(1));
+    //             });
+    //         }),
+    //         Thunk::new(|cb| {
+    //             thread::spawn(move || {
+    //                 thread::sleep(Duration::new(1, 0));
+    //                 cb(Ok(2));
+    //             });
+    //         }),
+    //         Thunk::new(|cb| {
+    //             thread::spawn(move || {
+    //                 thread::sleep(Duration::new(1, 0));
+    //                 cb(Ok(3));
+    //             });
+    //         })
+    //     ];
+    //     let res = Thunk::all(thunk_vec).await().unwrap();
+    //     assert_eq!(res, vec![1, 2, 3]);
+    // }
 }
